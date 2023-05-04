41a2538a9a2ab3d267b7bb9b9bc34231154a6674
Asheesh Gulati <kshemaka@users.noreply.github.com>
2023-05-04 13:10:54 +0200

    Update random_commander to handle game formats
    The "format" query parameter can be used to indicate one of the following formats: Commander (commander), Pauper Commander (paupercommander), Brawl (brawl), Pauper Brawl (pauperbrawl). The Commander format is assumed by default.

--- random_commander/main.py
+++ random_commander/main.py
@@ -8,7 +8,7 @@ import requests
 
 def retrieve_data():
     d = list()
-    query = '(game:paper) not:ub is:commander -o:partner -o:background'
+    query = '(game:paper) -st:funny not:ub is:brawlcommander -o:partner -o:background -t:background'
     url = 'https://api.scryfall.com/cards/search?q=' + urllib.parse.quote_plus(query)
     r = requests.get(url)
     while r.status_code == 200:
@@ -25,6 +25,37 @@ def retrieve_data():
 data = retrieve_data()
 
 
+def check_legality(card):
+    if card['legalities']['commander'] == 'banned':
+        return False
+    return True
+
+
+def check_type(card):
+    face = card
+    if 'card_faces' in card:
+        face = card['card_faces'][0]
+
+    if 'Planeswalker' in face['type_line'] and 'commander' not in face['oracle_text']:
+        return False
+    return True
+
+
+def check_rarity(card):
+    if 'uncommon' in card['rarity']:
+        return True
+    return False
+
+
+def filter_data(f):
+    d = data.copy()
+    if 'brawl' not in f:
+        d = [c for c in d if check_legality(c) and check_type(c)]
+    if 'pauper' in f:
+        d = [c for c in d if check_rarity(c)]
+    return d
+
+
 def random_commander(request: flask.Request) -> flask.Response:
     if request.method == 'OPTIONS':
         headers = {
@@ -36,7 +67,8 @@ def random_commander(request: flask.Request) -> flask.Response:
         return flask.Response(status=204, headers=headers)
 
     if len(data) > 0:
-        card = data[random.randrange(0, len(data))]
+        filtered_data = filter_data(request.args.get('format', 'commander'))
+        card = filtered_data[random.randrange(0, len(filtered_data))]
         response = flask.jsonify(card)
         response.headers['Access-Control-Allow-Origin'] = '*'
         return response
--- random_commander/test_main.py
+++ random_commander/test_main.py
@@ -12,10 +12,20 @@ class TestAPI(unittest.TestCase):
 
     def test_random_commander(self):
         with self.app.app_context():
-            request = unittest.mock.Mock()
+            request = unittest.mock.Mock(args={})
             response = main.random_commander(request)
             assert response.status_code == 200
             assert response.get_json()
+
+    def test_random_commander_Pauper(self):
+        with self.app.app_context():
+            request = unittest.mock.Mock(args={'format': 'pauperbrawl'})
+            response = main.random_commander(request)
+            assert response.status_code == 200
+
+            json = response.get_json()
+            assert json
+            assert json['rarity'] == 'uncommon'
 
 
 if __name__ == '__main__':
